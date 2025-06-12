87b658fbf88ed7915bba6e4ab6754820b352254d
muthuselvi <muthuselvi0976@gamil.com>
2025-06-12 21:51:12 +0530

    First version of My Finance Management System using SpringBoot

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     
+       <div className='min-h-screen bg-blue-200 p-6'>
+        <h1 className='text-3xl font-bold mb-4 text-center text-black'>Finance Management System</h1>
+        
+      </div>
+    
+    </>
+  )
+}
+
+export default App
